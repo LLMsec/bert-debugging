97ef63dcbc5d8c0138549e9a0c35708f74499c9d
Peter Lorenz <peter.lorenz@itwm.fraunhofer.de>
2023-09-24 22:22:34 +0200

    add task1.py

---
+++ task1.py
@@ -0,0 +1,240 @@
+import os
+import json
+import math
+from collections import OrderedDict
+import torch
+from torch import nn, Tensor
+from typing import Union, Tuple, List, Iterable, Dict
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from scipy.stats import pearsonr, spearmanr
+import numpy as np
+import gzip, csv
+import pandas as pd
+from tqdm.auto import tqdm
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+
+# %pip install transformers
+from transformers import AutoTokenizer
+# If you can not find all the bugs, use the line below for AutoModel
+#from transformers import AutoModel
+
+
+def gelu(x):
+    return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
+
+class Config(object):
+    def __init__(self,
+                vocab_size,
+                hidden_size=768,
+                num_hidden_layers=12,
+                num_attention_heads=12,
+                intermediate_size=3072,
+                dropout_prob=0.9,
+                max_position_embeddings=512,
+                type_vocab_size=2,
+                initializer_range=0.02):
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = dropout_prob
+        self.attention_probs_dropout_prob = dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+
+    @classmethod
+    def from_dict(cls, dict_object):
+        config = Config(vocab_size=None)
+        for (key, value) in dict_object.items():
+            config.__dict__[key] = value
+        return config
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, variance_epsilon=1e-12):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(hidden_size))
+        self.beta = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = variance_epsilon
+
+    def forward(self, x):
+        print("x", x.shape)
+        u = x.mean(0, keepdim=True)
+        s = (x + u).pow(2).mean(0, keepdim=True)
+        x = (x + u) / torch.sqrt(s + self.variance_epsilon)
+        return self.gamma * x + self.beta
+
+
+class MLP(nn.Module):
+    def __init__(self, hidden_size, intermediate_size):
+        super(MLP, self).__init__()
+        self.dense_expansion = nn.Linear(hidden_size, intermediate_size)
+        self.dense_contraction = nn.Linear(intermediate_size, hidden_size)
+
+    def forward(self, x):
+        x = self.dense_expansion(x)
+        x = self.dense_contraction(gelu(x))
+        return x
+
+
+class Layer(nn.Module):
+    def __init__(self, config):
+        super(Layer, self).__init__()
+
+        self.hidden_size = config.hidden_size
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+        self.attn_out = nn.Linear(config.hidden_size, config.hidden_size)
+        self.ln1 = LayerNorm(config.hidden_size)
+
+        self.mlp = MLP(config.hidden_size, config.intermediate_size)
+        self.ln2 = LayerNorm(config.hidden_size)
+
+    def split_heads(self, tensor, num_heads, attention_head_size):
+        new_shape = tensor.size()[:-1] + (num_heads, attention_head_size)
+        tensor = tensor.view(*new_shape)
+        return tensor.permute(0, 2, 1, 3)
+
+    def merge_heads(self, tensor, num_heads, attention_head_size):
+        tensor = tensor.permute(0, 2, 1, 3).contiguous()
+        new_shape = tensor.size()[:-2] + (num_heads * attention_head_size,)
+        return tensor.view(new_shape)
+
+    def attn(self, q, k, v, attention_mask):
+        mask = attention_mask == 1
+        mask = mask.unsqueeze(1).unsqueeze(2)
+
+        s = torch.matmul(q, k)
+        s = s / math.sqrt(self.attention_head_size)
+
+        s = torch.where(mask, s, torch.tensor(float('inf')))
+
+        p = s
+        p = self.dropout(p)
+
+        a = torch.matmul(p, v)
+        return a
+
+    def forward(self, x, attention_mask):
+        q, k, v = self.query(x), self.key(x), self.value(x)
+
+        q = self.split_heads(q, self.num_attention_heads, self.attention_head_size)
+        k = self.split_heads(k, self.num_attention_heads, self.attention_head_size)
+        v = self.split_heads(v, self.num_attention_heads, self.attention_head_size)
+
+        a = self.attn(q, k, v, attention_mask)
+        a = self.merge_heads(a, self.num_attention_heads, self.attention_head_size)
+        a = self.attn_out(a)
+        a = self.dropout(a)
+        a = self.ln1(a)
+
+        m = self.mlp(a)
+        m = self.dropout(m)
+        m = self.ln2(m)
+
+        return m
+
+
+class Bert(nn.Module):
+    def __init__(self, config_dict):
+        super(Bert, self).__init__()
+        self.config = Config.from_dict(config_dict)
+        self.embeddings = nn.ModuleDict({
+          'token': nn.Embedding(self.config.vocab_size, self.config.hidden_size, padding_idx=0),
+          'position': nn.Embedding(self.config.max_position_embeddings, self.config.hidden_size),
+          'token_type': nn.Embedding(self.config.type_vocab_size, self.config.hidden_size),
+        })
+
+
+        self.ln = LayerNorm(self.config.hidden_size)
+        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
+
+        self.layers = nn.ModuleList([
+            Layer(self.config) for _ in range(self.config.num_hidden_layers)
+        ])
+
+        self.pooler = nn.Sequential(OrderedDict([
+            ('dense', nn.Linear(self.config.hidden_size, self.config.hidden_size)),
+            ('activation', nn.Tanh()),
+        ]))
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, ):
+
+        print("input_ids", input_ids.shape)
+        # print("attention_mask", attention_mask.shape)
+        # print("token_type_ids", token_type_ids.shape)
+
+        position_ids = torch.arange(input_ids.size(1), dtype=torch.long, device=input_ids.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros_like(input_ids)
+
+        x = torch.cat((self.embeddings.token(input_ids),
+                       self.embeddings.position(position_ids),
+                       self.embeddings.token_type(token_type_ids)),
+                      dim=-1)
+        x = self.dropout(self.ln(x))
+
+        for layer in self.layers:
+            x = layer(x, attention_mask)
+
+        o = self.pooler(x[:, 0])
+        return (x, o)
+
+    def load_model(self, path):
+        self.load_state_dict(torch.load(path))
+        return self
+    
+
+    
+
+if __name__ == "__main__":
+    MODEL_NAME = 'prajjwal1/bert-tiny'
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    #EXAMPLE USE
+    sentence = 'An example use of pretrained BERT with transformers library to encode a sentence'
+    tokenized_sample = tokenizer(sentence, return_tensors='pt', padding='max_length', max_length=512)
+
+    path_bin = 'bert_tiny.bin'
+    check_file = os.path.isfile(path_bin)
+    if not check_file:
+        print("!wget https://github.com/for-ai/bert/raw/master/bert_tiny.bin")
+
+
+    MODEL_NAME = 'prajjwal1/bert-tiny'
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    ## IF YOU CANNOT SOLVE PREVIOUS BUGS, USE THE LINE BELOW:
+    ## bert = AutoModel.from_pretrained(MODEL_NAME)
+
+    bert_config = {"hidden_size": 128, "num_attention_heads": 2, "num_hidden_layers": 2, "intermediate_size": 512, "vocab_size": 30522}
+    
+    bert = Bert(bert_config).load_model(path_bin)
+    
+    
+    output = bert(input_ids=tokenized_sample['input_ids'],  attention_mask=tokenized_sample['attention_mask'], token_type_ids=tokenized_sample["token_type_ids"],)
+    
+    # We use "pooler_output" for simplicity. This corresponds the last layer
+    # hidden-state of the first token of the sequence (CLS token) after
+    # further processing through the layers used for the auxiliary pretraining task.
+    embedding = output[1]
+    print(f'\nResulting embedding shape: {embedding.shape}')
